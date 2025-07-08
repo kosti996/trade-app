77cad9209b5df3565e84c5d70d9306a5f318652b
kosti996 <kostiukevycha@gmail.com>
2025-07-08 12:36:02 +0200

    changed tab styles to material

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-tabs',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss'
 })
